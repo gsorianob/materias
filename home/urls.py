464c5209a14dfa4399bd650bc79201198526f74c
Gonzalo Soriano <gsorianob@gmail.com>
2013-10-16 23:07:23 -0300

    + Agrego el sticky footer

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('home',
-    url(r'^$', TemplateView.as_view(template_name='index.html'),  name='index'),
+   url(r'^$', TemplateView.as_view(template_name='index.html'),  name='index'),
+   url(r'^index$', TemplateView.as_view(template_name='index.html')),
+   url(r'^contact$', TemplateView.as_view(template_name='contact.html'),  name='contact'),
+   url(r'^aboutus$', TemplateView.as_view(template_name='aboutus.html'),  name='aboutus'),
 )
